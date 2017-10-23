e52a133917a346961c0cc53f7d99e3dbac77dc58
franpsif <franpsif@gmail.com>
2017-10-24 00:15:43 +0200

    Removed the main container since it is not needed because the login page has been removed. Moved the main container content to the app component. Put the header satatic at the top. Modified the routing module in order to not use the main container.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { MainContainerComponent } from './core/main-container/main-container.component';
 import { DesignerComponent } from './designer/designer.component';
 import { ExerciseDetailComponent } from './routines/exercise-detail/exercise-detail.component';
 import { RoutinesComponent } from './routines/routines.component';
@@ -6,11 +5,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-    {path: '', component: MainContainerComponent, children: [
-        {path: 'designer', component: DesignerComponent},
-        {path: 'routines', component: RoutinesComponent, children: [
-            {path: ':exercise', component: ExerciseDetailComponent}
-        ]},
+    {path: 'designer', component: DesignerComponent},
+    {path: 'routines', component: RoutinesComponent, children: [
+        {path: ':exercise', component: ExerciseDetailComponent}
     ]},
     {path: '**', redirectTo: 'designer'}
 ];
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,12 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './../app-routing.module';
 import { SharedModule } from './../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
-import { MainContainerComponent } from './main-container/main-container.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
-        HeaderComponent,
-        MainContainerComponent
+        HeaderComponent
     ],
     imports: [
         SharedModule,
